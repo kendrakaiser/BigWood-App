b1882cb016c07f19592c6d1e1c3c57812a035ad6
Sam Carlson <sam.p.carlson@gmail.com>
2022-09-02 12:02:14 -0600

    complete nonsense

--- functions.r
+++ functions.r
@@ -1,20 +1,10 @@
-scdbConnect=function(readOnly=T){
-  if(readOnly){
-    conn=dbConnect(RPostgres::Postgres(),
-                   host="silvercreekdb-do-user-12108041-0.b.db.ondigitalocean.com",
-                   port="25060",
-                   dbname="silvercreekdb" ,
-                   user="dbread",
-                   password=Sys.getenv("scdb_readPass")
-    )
-  } else {
-    conn=dbConnect(RPostgres::Postgres(),
-                   host="silvercreekdb-do-user-12108041-0.b.db.ondigitalocean.com",
-                   port="25060",
-                   dbname="silvercreekdb" ,
-                   user="dbwrite",
-                   password=Sys.getenv("scdb_pass")
-    )
-  }
+scdbConnect=function(){
+  conn=dbConnect(RPostgres::Postgres(),
+                 host="silvercreekdb-do-user-12108041-0.b.db.ondigitalocean.com",
+                 port="25060",
+                 dbname="silvercreekdb" ,
+                 user="dbread",
+                 password="dbread"
+  )
   return(conn)
 }
