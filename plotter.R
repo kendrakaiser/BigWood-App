d6c5a02a2238d369f6b4ef0bc2562cd67e027d08
Steven Schmitz <120681809+sjschmitz@users.noreply.github.com>
2024-02-16 14:45:10 -0700

    Update plotter.R

--- plotter.R
+++ plotter.R
@@ -155,65 +155,59 @@ ex.vols3 <- exc_prob %>%pivot_longer(!Exceedance, names_to="site", values_to="va
 #-------------------------------------------------------------------------------------#
 colfunc<-colorRampPalette(c("red","darkorange","green3","deepskyblue", "blue3"))
 
-p<-ggplot(vol.big, fill=t, aes(x=site, y=value, fill=site), alpha=0.6) +
-  geom_boxplot(outlier.alpha = 0.3) +
-  scale_fill_manual(values=c("royalblue3", "grey90","royalblue3", "grey90")) +
-  scale_x_discrete(labels = function(x) str_wrap(x, width = 10))+
-  scale_y_continuous(breaks = round(seq(0, max(vol.big$value, na.rm=TRUE), by = 50),1))+
+gen_bw <- function(vol.big, ex.vols3){
+  p<-ggplot(vol.big, fill=t, aes(x=site, y=value, fill=site), alpha=0.6) +
+    geom_boxplot(outlier.alpha = 0.3) +
+    scale_fill_manual(values=c("royalblue3", "grey90","royalblue3", "grey90")) +
+    scale_x_discrete(labels = function(x) str_wrap(x, width = 10))+
+    scale_y_continuous(breaks = round(seq(0, max(vol.big$value, na.rm=TRUE), by = 50),1))+
   
-  geom_point(data=ex.vols3[ex.vols3$site !="Silver Creek" & ex.vols3$site !="Camas Creek",], aes(x=site, y=value, color=as.factor(Exceedance)), size=2, shape=21)+
-  scale_color_manual(values=c("black","black","black","black","black"))+
-  theme_bw(base_size = 14)+
-  ggtitle("Historic & Modeled Irrigation Season Volumes (April-Sept.)") +
-  labs(fill="", color="Exceedance")+
-  guides(fill = "none", color = "none") +
-  xlab("")+
-  ylab("Irrigation Season Volume (KAF)") 
+    geom_point(data=ex.vols3[ex.vols3$site !="Silver Creek" & ex.vols3$site !="Camas Creek",], aes(x=site, y=value, color=as.factor(Exceedance)), size=2, shape=21)+
+    scale_color_manual(values=c("black","black","black","black","black"))+
+    theme_bw(base_size = 14)+
+    ggtitle("Historic & Modeled Irrigation Season Volumes (April-Sept.)") +
+    labs(fill="", color="Exceedance")+
+    guides(fill = "none", color = "none") +
+    xlab("")+
+    ylab("Irrigation Season Volume (KAF)")
 
-png(filename = file.path("www/sampled_vol_bw.png"),
-    width = 6.5, height = 5.5,units = "in", pointsize = 12,
-    bg = "white", res = 600) 
-print(p)
-dev.off()
+  return(p)
+}
 
-ps<- ggplot(vol.sm, fill =t, aes(x=site, y=value, fill=site), alpha=0.6) +
-  geom_boxplot(outlier.alpha = 0.3) +
-  scale_fill_manual(values=c("royalblue3", "grey90", "royalblue3", "grey90")) +
-  scale_x_discrete(labels = function(x) str_wrap(x, width = 10))+
-  scale_y_continuous(breaks = round(seq(0, max(vol.sm$value, na.rm=TRUE), by = 10),1))+
-  geom_point(data=ex.vols3[ex.vols3$site =="Silver Creek",], aes(x=site, y=value, color=as.factor(Exceedance)), size=2, shape=21)+
-  scale_color_manual(values=c("black","black","black","black","black"))+
-  theme_bw(base_size = 18) +
-  ggtitle("") +
-  labs(fill="", color="Exceedance")+
-  guides(fill = "none", color = "none") +
-  xlab("")+
-  ylab("Irrigation Volume (KAF)")
+gen_sc <- function(vol.sm,ex.vols3){
+  ps<- ggplot(vol.sm, fill =t, aes(x=site, y=value, fill=site), alpha=0.6) +
+    geom_boxplot(outlier.alpha = 0.3) +
+    scale_fill_manual(values=c("royalblue3", "grey90", "royalblue3", "grey90")) +
+    scale_x_discrete(labels = function(x) str_wrap(x, width = 10))+
+    scale_y_continuous(breaks = round(seq(0, max(vol.sm$value, na.rm=TRUE), by = 10),1))+
+    
+    geom_point(data=ex.vols3[ex.vols3$site =="Silver Creek",], aes(x=site, y=value, color=as.factor(Exceedance)), size=2, shape=21)+
+    scale_color_manual(values=c("black","black","black","black","black"))+
+    theme_bw(base_size = 18) +
+    ggtitle("") +
+    labs(fill="", color="Exceedance")+
+    guides(fill = "none", color = "none") +
+    xlab("")+
+    ylab("Irrigation Volume (KAF)")
+  
+  return(ps)
+}
 
-png(filename = file.path("www/sampled_vol_sc.png"),
-    width = 5.5, height = 5.5,units = "in", pointsize = 12,
-    bg = "white", res = 600) 
-print(ps)
-dev.off()
-
-pc<- ggplot(vol.cc, fill = t, aes(x=site, y=value, fill=site), alpha=0.6) +
-  geom_boxplot(outlier.alpha = 0.3) +
-  scale_fill_manual(values=c("royalblue3", "grey90", "royalblue3", "grey90"))+
-  scale_x_discrete(labels = function(x) str_wrap(x, width = 10))+
-  scale_y_continuous(breaks = round(seq(0, max(vol.cc$value, na.rm=TRUE), by = 40),1))+
-  geom_point(data=ex.vols3[ex.vols3$site =="Camas Creek",], aes(x=site, y=value, color=as.factor(Exceedance)), size=2, shape=21)+
-  scale_color_manual(values=c("black","black","black","black","black"))+
-  theme_bw(base_size = 18)+
-  ggtitle("") +
-  labs(fill="", color="Exceedance")+
-  guides(fill = "none", color = "none")+
-  xlab("")+
-  ylab("Irrigation Volume (KAF)")
-
-png(filename = file.path("www/sampled_vol_cc.png"),
-    width = 5.5, height = 5.5,units = "in", pointsize = 12,
-    bg = "white", res = 600) 
-print(pc)
-dev.off()
-print(pc)
-dev.off()
+gen_cc <- function(vol.cc, exvols3){
+  pc<- ggplot(vol.cc, fill = t, aes(x=site, y=value, fill=site), alpha=0.6) +
+    geom_boxplot(outlier.alpha = 0.3) +
+    scale_fill_manual(values=c("royalblue3", "grey90", "royalblue3", "grey90"))+
+    scale_x_discrete(labels = function(x) str_wrap(x, width = 10))+
+    scale_y_continuous(breaks = round(seq(0, max(vol.cc$value, na.rm=TRUE), by = 40),1))+
+    
+    geom_point(data=ex.vols3[ex.vols3$site =="Camas Creek",], aes(x=site, y=value, color=as.factor(Exceedance)), size=2, shape=21)+
+    scale_color_manual(values=c("black","black","black","black","black"))+
+    theme_bw(base_size = 18)+
+    ggtitle("") +
+    labs(fill="", color="Exceedance")+
+    guides(fill = "none", color = "none")+
+    xlab("")+
+    ylab("Irrigation Volume (KAF)")
+  
+  return(pc)
+}
